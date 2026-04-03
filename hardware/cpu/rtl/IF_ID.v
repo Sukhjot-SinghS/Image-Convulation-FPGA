7bc7754a7125e402d606fbfd2dc25046119862ae
Sukhjot-SinghS <rajpreet.js459@gmail.com>
2026-04-03 10:14:13 +0530

    Fixed majority logic and did exhaustive testing to finalise correct code

--- hardware/cpu/rtl/IF_ID.v
+++ hardware/cpu/rtl/IF_ID.v
@@ -68,7 +68,9 @@ reg [31:0] saved_instruction;
     end
 
     // 3. YOUR MODIFIED WIRE: If we saved an instruction, output it. Otherwise, output RAM.
-    assign instruction_o = has_saved_inst ? saved_instruction : (stall_read_i ? NOP : inst_mem_read_data);
+    assign instruction_o = has_saved_inst ? saved_instruction : ((stall_read_i ) ? NOP : inst_mem_read_data);
+
+
 ////////////////////////////////////////////////////////////// Exception detection////////////////////////////////////////////////////////////
 always @(posedge clk or negedge reset) begin
     if (!reset)
@@ -106,11 +108,11 @@ always @(*) begin
 end
 // change 1 
 
-id_ex_reg u_id_ex (.clk        	(clk),.reset_n    	(reset),.stall_n    	(stall_read_i),
+id_ex_reg u_id_ex (.clk        	(clk),.reset_n    	(reset),.stall_i    	(stall_read_i),
 // From ID
 	.immediate_i	(immediate),
 	.immediate_sel_i((instruction_i[`OPCODE] == JALR)  ||(instruction_i[`OPCODE] == LOAD)  ||(instruction_i[`OPCODE] == ARITHI) ),
-	.alu_i      	((instruction_i[`OPCODE] == ARITHI || instruction_i[`OPCODE] == ARITHR) && instruction_i[25] != 1'b1),
+	.alu_i      	((instruction_i[`OPCODE] == ARITHI) || (instruction_i[`OPCODE] == ARITHR && instruction_i[31:25] != 7'b0000001)),
 	.lui_i      	(instruction_i[`OPCODE] == LUI),
 	.jal_i      	(instruction_i[`OPCODE] == JAL),
 	.jalr_i     	(instruction_i[`OPCODE] == JALR),
@@ -124,7 +126,7 @@ id_ex_reg u_id_ex (.clk        	(clk),.reset_n    	(reset),.stall_n    	(stall_r
 	.dest_reg_sel_i (instruction_i[`RD]),
 	.alu_op_i   	(instruction_i[`FUNC3]),
 	.illegal_inst_i (illegal_inst),
-    .is_mext_i(instruction_i[`OPCODE] == ARITHR && instruction_i[25] == 1'b1),
+    .is_mext_i(instruction_i[`OPCODE] == ARITHR && instruction_i[31:25] == 7'b0000001),
 
 	// To EX (WIRES)
 	.execute_immediate_o (execute_immediate_w),
@@ -153,7 +155,7 @@ endmodule
 module id_ex_reg (
     input         clk,
     input         reset_n,
-    input         stall_n,
+    input         stall_i,
 
     // Inputs from ID
     input  [31:0] immediate_i,
@@ -214,7 +216,7 @@ always @(posedge clk or negedge reset_n) begin
         illegal_inst_o      <= 1'b0;
         is_mext_o <= 1'b0;
     end
-    else if (!stall_n) begin
+    else if (!stall_i) begin
         execute_immediate_o <= immediate_i;
         immediate_sel_o     <= immediate_sel_i;
         alu_o               <= alu_i;
--- hardware/top/top_fpga.v
+++ hardware/top/top_fpga.v
@@ -12,14 +12,10 @@ output [15:0] led
     
 );
 
-wire [15:0] pc_disp = pipe_u.inst_fetch_pc[15:0];
+assign [15:0] led = pipe_u.inst_fetch_pc[15:0];
 wire exception;
-	////////////////////////////////////////////////////////////
-	// Slow clock generator (clock divider)
-	////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////
-// 100 MHz → 1 Hz clock divider (Nexys A7)
-////////////////////////////////////////////////////////////
+
+
 reg [25:0] clk_cnt;   	// enough for 50 million
 reg    	slow_clk;
 
@@ -51,24 +47,38 @@ end
 	assign inst_mem_is_valid = 1'b1;
 	assign dmem_write_valid  = 1'b1;
 	assign dmem_read_valid   = 1'b1;
-assign led = pc_disp;
+
+	wire exception;
+	wire [31:0]pc_out;
+
+	wire        dmem_re, dmem_we;
+	wire [31:0] dmem_raddr, dmem_waddr, dmem_wdata, dmem_rdata;
+	wire [3:0]  dmem_wstrb;
 
 ////////////////////////////////////////////////////////////
 // PIPELINE CPU
 ////////////////////////////////////////////////////////////
-pipe pipe_u (
-	.clk(clk),
+pipe DUT (
+	.clk(slow_clk),
 	.reset(reset),
 	.stall(1'b0),
 	.exception(exception),
 
 	.inst_mem_is_valid(inst_mem_is_valid),
 	.inst_mem_read_data(inst_mem_read_data),
+	.pc_out(pc_out),
 
 	.dmem_read_data_temp(dmem_read_data),
 	.dmem_write_valid(dmem_write_valid),
-	.dmem_read_valid(dmem_read_valid)
-// TODO: Might have a few more port signals
+	.dmem_read_valid(dmem_read_valid),
+
+	.dmem_re_o    (dmem_re),
+    .dmem_raddr_o (dmem_raddr),
+    .dmem_we_o    (dmem_we),
+    .dmem_waddr_o (dmem_waddr),
+    .dmem_wdata_o (dmem_wdata),
+    .dmem_wstrb_o (dmem_wstrb),
+    .dmem_rdata_i (dmem_rdata)
 );
 
 
@@ -77,7 +87,7 @@ pipe pipe_u (
 ////////////////////////////////////////////////////////////
 instr_mem IMEM (
 	.clk(clk),
-	.pc(TODO: Add inst_mem_address as a port signal from the pipe),
+	.pc(pc_out),
 	.instr(inst_mem_read_data)
 );
 
@@ -86,16 +96,14 @@ instr_mem IMEM (
 // DATA MEMORY  (matches data_mem.v)
 ////////////////////////////////////////////////////////////
 data_mem DMEM (
-	.clk(clk),
-
-	.re(TODO: Add dmem_read_ready as a port signal from the pipe),
-	.raddr(TODO),
-	.rdata(dmem_read_data),
-
-	.we(TODO: Add dmem_write_ready as a port signal from the pipe),
-	.waddr(TODO),
-	.wdata(TODO),
-	.wstrb(TODO: Add dmem_write_byte as a port signal from the pipe)
+    .clk   (slow_clk),
+    .re    (dmem_re),
+    .raddr (dmem_raddr),
+    .rdata (dmem_read_data),
+    .we    (dmem_we),
+    .waddr (dmem_waddr),
+    .wdata (dmem_wdata),
+    .wstrb (dmem_wstrb)
 );
 
 
